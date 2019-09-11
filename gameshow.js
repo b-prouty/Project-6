201430875d2b5ed4d486f17d3c7a911c6529f2fc
Brian Prouty <brianproutydesign@gmail.com>
2019-09-11 12:56:02 -0700

    Added phrases to display

--- gameshow.js
+++ gameshow.js
@@ -5,32 +5,38 @@ const overlay = document.getElementById('overlay');
 const startButton =
 document.querySelector('a.btn__reset');
 const phrases = [
-  ['A fish out of water'],
-  ['Break a leg'],
-  ['Cat got your tongue'],
-  ['High and dry'],
-  ['The whole shebang']
+  'A fish out of water',
+  'Break a leg',
+  'Cat got your tongue',
+  'High and dry',
+  'The whole shebang'
 ];
-
 
 startButton.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
 
-function getRandomPhrasesAsArray(array) {
-  var randomArray = array[Math.floor( Math.random() * array.length)];
-  return randomArray;
+function getRandomPhraseAsArray(phrases) {
+  const randomArray = phrases[(Math.floor( Math.random() * phrases.length))];
+  const characterArray = randomArray.split('');
+  return characterArray;
 }
 
-function addPhraseToDisplay(arr){
+function addPhraseToDisplay(arr) {
+  const ul = phrase.firstElementChild;
   for (let i = 0; i < arr.length; i++) {
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    const character = i.textContent;
+    let character = arr[i];
+    let li = document.createElement('li');
     li.textContent = character;
     ul.appendChild(li);
+    if (character === ' '){
+      li.className = '';
+    } else {
+      li.className = 'letter';
+    }
   }
 }
 
 
-const phraseArray = getRandomPhrasesAsArray(phrases);
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
